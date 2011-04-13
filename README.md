a208d0f39b1a2498fbade342754a6c2be8b21cc8
Emmanuel Vella <vella.emmanuel@gmail.com>
2011-04-13 10:33:26 +0200

    added main files

---
+++ README.md
@@ -0,0 +1,1 @@
+Add the HTML5 / CSS3 52framework inside the Symfony2 framework.
---
+++ S2frameworkBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EmmanuelVella\S2frameworkBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EmmanuelVellaS2frameworkBundle extends Bundle
+{
+
+}
